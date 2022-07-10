63cca407f5b9cfcc6373982f3b63db8768c0cbc0
subin <200516bb@gmail.com>
2022-07-10 22:16:06 +0900

    소통 게시판 메인화면 구성

--- app/src/main/java/com/example/petmilyandroid/ui/main/communication/CommunicationFragment.kt
+++ app/src/main/java/com/example/petmilyandroid/ui/main/communication/CommunicationFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.flo.LogGridRVAdapter
 import com.example.petmilyandroid.databinding.FragmentCommunicationBinding
 
 class CommunicationFragment : Fragment() {
     lateinit var binding: FragmentCommunicationBinding
+    private var list = ArrayList<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +20,17 @@ class CommunicationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCommunicationBinding.inflate(inflater, container, false)
+
+
+        list.add("고양이")
+        list.add("강아지")
+        list.add("멈멈")
+        list.add("인절미")
+        list.add("백구")
+        list.add("거부기")
+        val logGridRVAdapter = LogGridRVAdapter(list)
+        binding.communicationLogRv.adapter = logGridRVAdapter
+        binding.communicationLogRv.layoutManager = GridLayoutManager(context, 2)
 
         return binding.root
     }
---
+++ app/src/main/java/com/example/petmilyandroid/ui/main/communication/LogGridRVAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.flo
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.petmilyandroid.databinding.ItemLogGridBinding
+
+class LogGridRVAdapter(private var list: ArrayList<String>): RecyclerView.Adapter<LogGridRVAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): LogGridRVAdapter.ViewHolder { //ViewHolder를 만들 때 나오는 함수
+        val binding: ItemLogGridBinding = ItemLogGridBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: LogGridRVAdapter.ViewHolder, position: Int) { // 데이터 바인딩 작업할 때마다 불러오는 함수
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int = list.size
+    //리사이클러 뷰의 마지막이 언제인지
+
+    inner class ViewHolder(val binding: ItemLogGridBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(name: String){
+            binding.itemProfileNameTv.text = name
+        }
+    }
+}
